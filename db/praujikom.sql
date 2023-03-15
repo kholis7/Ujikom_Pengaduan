28ca5ee0d76952d1f1dcdd4969a2e9039f850b56
Kholis_7 <kholissetia17@gmail.com>
2023-03-15 20:14:14 +0700

    Database
    Database Ujikom RPL Pengaduan Masyarakat

---
+++ db/praujikom.sql
@@ -0,0 +1,172 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 15, 2023 at 02:13 PM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `praujikom`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_masyarakat`
+--
+
+CREATE TABLE `tb_masyarakat` (
+  `nik` varchar(16) NOT NULL,
+  `nama` varchar(35) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `telp` varchar(13) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_masyarakat`
+--
+
+INSERT INTO `tb_masyarakat` (`nik`, `nama`, `username`, `password`, `telp`) VALUES
+('12121625611', 'Fazal', 'fajal', '123', '127182121'),
+('12121625612', 'Cadnra', 'candra', '123', '0897263512'),
+('12121625613', 'Pajli', 'pajli', '123', '89218291728'),
+('21213233431', 'nur', 'nur', '123', '73827832763'),
+('3212150607970012', 'dar', 'dar', '123', '0897263512211'),
+('3212190202970001', 'Nur Kholis', 'masyarakat', 'masyarakat', '0827127182617');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_pengaduan`
+--
+
+CREATE TABLE `tb_pengaduan` (
+  `id_pengaduan` int(11) NOT NULL,
+  `tgl_pengaduan` date NOT NULL,
+  `nik` varchar(16) NOT NULL,
+  `isi_laporan` text NOT NULL,
+  `foto` varchar(255) NOT NULL,
+  `status` enum('0','Proses','Selesai') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_pengaduan`
+--
+
+INSERT INTO `tb_pengaduan` (`id_pengaduan`, `tgl_pengaduan`, `nik`, `isi_laporan`, `foto`, `status`) VALUES
+(20, '2023-03-07', '12121625612', '   saasa', 'smp.jpeg', 'Selesai'),
+(21, '2023-03-01', '12121625611', 'saswewe', 'smp.jpeg', 'Proses');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_petugas`
+--
+
+CREATE TABLE `tb_petugas` (
+  `id_petugas` int(11) NOT NULL,
+  `nama_petugas` varchar(35) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `telp` varchar(13) NOT NULL,
+  `level` enum('admin','petugas') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_petugas`
+--
+
+INSERT INTO `tb_petugas` (`id_petugas`, `nama_petugas`, `username`, `password`, `telp`, `level`) VALUES
+(1, 'Admin', 'admin', 'admin', '0827127182617', 'admin'),
+(2, 'SMK YAPIIM', 'petugas', 'petugas', '0829127186271', 'petugas');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_tanggapan`
+--
+
+CREATE TABLE `tb_tanggapan` (
+  `id_tanggapan` int(11) NOT NULL,
+  `id_pengaduan` int(11) NOT NULL,
+  `tgl_tanggapan` date NOT NULL,
+  `tanggapan` text NOT NULL,
+  `id_petugas` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_tanggapan`
+--
+
+INSERT INTO `tb_tanggapan` (`id_tanggapan`, `id_pengaduan`, `tgl_tanggapan`, `tanggapan`, `id_petugas`) VALUES
+(1, 1, '2023-03-12', ' Foto Tidak Sesuai', 0),
+(12, 20, '2023-03-12', 'Untuk laporan lampu mati sudah diatasi oleh aparat desa dan warga sekitar, terimakasih atas laporan yang anda berikan, jika ada masalah yang lain silahkan laporkan kembali', 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_masyarakat`
+--
+ALTER TABLE `tb_masyarakat`
+  ADD PRIMARY KEY (`nik`);
+
+--
+-- Indexes for table `tb_pengaduan`
+--
+ALTER TABLE `tb_pengaduan`
+  ADD PRIMARY KEY (`id_pengaduan`);
+
+--
+-- Indexes for table `tb_petugas`
+--
+ALTER TABLE `tb_petugas`
+  ADD PRIMARY KEY (`id_petugas`);
+
+--
+-- Indexes for table `tb_tanggapan`
+--
+ALTER TABLE `tb_tanggapan`
+  ADD PRIMARY KEY (`id_tanggapan`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_pengaduan`
+--
+ALTER TABLE `tb_pengaduan`
+  MODIFY `id_pengaduan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT for table `tb_petugas`
+--
+ALTER TABLE `tb_petugas`
+  MODIFY `id_petugas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `tb_tanggapan`
+--
+ALTER TABLE `tb_tanggapan`
+  MODIFY `id_tanggapan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
